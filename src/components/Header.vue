f53fce25a2b77b788a08527d08300b3fdd95fbcf
Jessa <jessa@mallowfields.com>
2021-07-30 19:03:01 -0400

    adds affirmations

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,32 @@
 <template>
   <div>
-    <v-card-title class="py-1 mx-6 d-flex justify-space-between">
+    <v-card-title class="py-1 mx-6 d-flex justify-space-between align-center">
       <h1 class="font-weight-bold">
-        <router-link :to="'/'" name="home" class="nav-links page-title">jessa challa</router-link>
+        <router-link :to="'/'" name="home" class="nav-links page-title"
+          >jessa challa</router-link
+        >
       </h1>
+      <v-list class="nav-bar nav-text">
+        <router-link
+          :to="'/introduction'"
+          name="introduction"
+          class="nav-links"
+        >
+          <v-list-item class="px-8">Introduction</v-list-item>
+        </router-link>
+        <router-link :to="'/experience'" name="experience" class="nav-links">
+          <v-list-item class="px-8"> Experience </v-list-item>
+        </router-link>
+        <router-link :to="'/gallery'" name="gallery" class="nav-links">
+          <v-list-item class="px-8">Gallery</v-list-item>
+        </router-link>
+        <router-link :to="'/news'" name="news" class="nav-links">
+          <v-list-item class="px-8">In the News</v-list-item>
+        </router-link>
+        <router-link :to="'/contact'" name="contact" class="nav-links">
+          <v-list-item class="px-8">Contact</v-list-item>
+        </router-link>
+      </v-list>
       <div>
         <v-btn v-for="icon in icons" :key="icon" icon>
           <v-icon size="24px" color="orange">
@@ -12,24 +35,6 @@
         </v-btn>
       </div>
     </v-card-title>
-
-    <v-list class="nav-bar">
-      <router-link :to="'/introduction'" name="introduction" class="nav-links">
-        <v-list-item> Introduction </v-list-item>
-      </router-link>
-      <router-link :to="'/experience'" name="experience" class="nav-links">
-        <v-list-item> Experience </v-list-item>
-      </router-link>
-      <router-link :to="'/gallery'" name="gallery" class="nav-links">
-        <v-list-item> Gallery </v-list-item>
-      </router-link>
-      <router-link :to="'/news'" name="news" class="nav-links">
-        <v-list-item> In the News </v-list-item>
-      </router-link>
-      <router-link :to="'/contact'" name="contact" class="nav-links">
-        <v-list-item> Contact </v-list-item>
-      </router-link>
-    </v-list>
   </div>
 </template>
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,56 @@
 <template>
-  <v-container> Yo </v-container>
+  <div class="content-container">
+    <v-card
+      flat
+      class="content-body d-flex flex-column align-center justify-center"
+    >
+      <h1>Welcome!</h1>
+      I am glad you are here! Take time to splash around...
+      <div
+        @click="splash"
+        class="
+          pool
+          d-flex
+          flex-column
+          justify-center
+          align-center align-self-center
+        "
+      >
+        <div class="affirmation">{{ affirmation }}</div>
+      </div>
+    </v-card>
+  </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    affirmation: '',
+    affirmations: [
+      'Small progress is still progress.',
+      'The path to success is to take massive, determined action.',
+      'I give myself permission to do what is right for me.',
+      'It is not a sprint, it is a marathon. One step at a time.',
+      "I'm rooting for you.",
+      "You're an inspiration.",
+      'You are worthy and deserving of respect.',
+      'Life happens.',
+      'Your mind is full of brilliant ideas.',
+      'Give yourself the space to grow and learn.',
+      'Reflect on what is truely fulfilling to you.',
+      'Accept your emotions and let them serve their purpose.',
+      'Save your energy for things that matter to you.',
+      'You are becoming closer to your true self every day.',
+      'Take time for peace.',
+      'Take time for fun.'
+    ]
+  }),
+  methods: {
+    splash () {
+      var affirmation =
+        this.affirmations[Math.floor(Math.random() * this.affirmations.length)]
+      this.affirmation = affirmation
+    }
+  }
+}
+</script>
