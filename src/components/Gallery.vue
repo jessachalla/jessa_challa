a77f5cc766c68ee725785af4222f4cd22ca3e9ef
Jessa <jessa@mallowfields.com>
2021-08-03 16:44:53 -0400

    adds to in the news

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,6 +1,20 @@
 <template>
   <v-container>
     <a :href="link">{{ linkText }}</a>
+    <v-carousel
+      cycle
+      height="400"
+      hide-delimiter-background
+      show-arrows-on-hover
+    >
+      <v-carousel-item v-for="(slide, i) in slides" :key="i">
+        <v-sheet :color="colors[i]" height="100%">
+          <v-row class="fill-height" align="center" justify="center">
+            <div class="text-h2">{{ slide }} Slide</div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
   </v-container>
 </template>
 
@@ -8,7 +22,15 @@
 export default {
   data: () => ({
     link: 'https://arcg.is/1viKzz0',
-    linkText: 'Grand Rapids Area Minority Owned Businesses'
+    linkText: 'Grand Rapids Area Minority Owned Businesses',
+    colors: [
+      'indigo',
+      'warning',
+      'pink darken-2',
+      'red lighten-1',
+      'deep-purple accent-4'
+    ],
+    slides: ['First', 'Second', 'Third', 'Fourth', 'Fifth']
   })
 }
 </script>
