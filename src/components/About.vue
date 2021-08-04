10b05e803defce9bdb17aa6fbe7b6b31a3f971d3
Jessa <jessa@mallowfields.com>
2021-08-04 13:30:05 -0400

    updates about page and image layout

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,64 +1,55 @@
 <template>
-  <v-container>
-    <div class="jessa d-flex flex-column">
-      <v-row>
-        <v-col cols="8">
-          <p class="intro-p mt-6">
-          <span class="flair">Hey there! </span> I'm Jessa Challa. I'm a lifelong
-          Grand Rapids resident and equity advocate. I'm Co-CEO and a Software
-          Engineer at a minority and women-owned software company,
-          <span class="flair">Mallowfields, LLC</span>.
-        </p>
-        <p class="intro-p">
-          I graduated from <span class="flair">Aquinas College</span> with a
-          degree in Geography. My focus on geography and Geographic Information
-          Systems (GIS) opened doors for impact in local government and the
-          nonprofit sector including work with the Grand Valley Metro Council,
-          Grand Rapids Public Museum, and the City of Grand Rapids Engineering
-          Department and Parking Department.
-        </p>
-        <p class="intro-p">
-          I graduated from <span class="flair">Aquinas College</span> with a
-          degree in Geography. My focus on geography and Geographic Information
-          Systems (GIS) opened doors for impact in local government and the
-          nonprofit sector including work with the Grand Valley Metro Council,
-          Grand Rapids Public Museum, and the City of Grand Rapids Engineering
-          Department and Parking Department.
-        </p>
-        </v-col>
-        <v-col cols="4">
-          <v-img
-            lazy-src="images/jessa_cutout.jpg"
-            src="images/jessa_cutout.jpg"
-          ></v-img>
-        </v-col>
-        <v-row>
-          <v-col class="ma-5">
-          <p class="intro-p">
-            So how did I become a Software Engineer? Well my tech journey was
-            non-traditional: in 2019, I completed a coding bootcamp at
-            <span class="flair">Grand Circus</span>. Now, I teach free coding
-            classes through Grand Circus, mentor and connect womxn and BIPOC
-            individuals to opportunities in the tech sector, and maintain a high
-            level of excellence within my field as a successful BIPOC woman in my
-            field. I have the ultimate goal of making the tech sector a more
-            inviting and inclusive place for all.
-          </p>
-          <p class="intro-p">
-            And what about community work? Well, as a member of the 2020 Cohort of
-            Community Catalysts through
-            <span class="flair">WMCAT’s Public Agency</span>, I've utilized
-            Equity-Centered Design to begin the work to challenge and change systems
-            and institutions of power to be more equitable across Grand Rapids
-            through strategic projects and initiatives. I also work on personal
-            community project to create community resources.
-          </p>
-          </v-col>
-        </v-row>
-      </v-row>
-      <p class="intro-p flair ml-auto">
+  <v-container class="about">
+    <div class="about-ps">
+      <v-img
+        lazy-src="images/jessa_cutout.jpg"
+        src="images/jessa_cutout.jpg"
+        class="jessa"
+      ></v-img>
+      <p class="intro-p mt-4">
+        <span class="flair">Hey there! </span> I'm Jessa Challa. I'm a lifelong
+        Grand Rapids resident and equity advocate. I'm Co-CEO and a Software
+        Engineer at a minority and women-owned software company,
+        <span class="flair">Mallowfields, LLC</span>.
+      </p>
+      <p class="intro-p">
+        I graduated from <span class="flair">Aquinas College</span> with a
+        degree in Geography. My focus on geography and Geographic Information
+        Systems (GIS) opened doors for impact in local government and the
+        nonprofit sector including work with the Grand Valley Metro Council,
+        Grand Rapids Public Museum, and the City of Grand Rapids Engineering
+        Department and Parking Department.
+      </p>
+      <p class="intro-p">
+        I graduated from <span class="flair">Aquinas College</span> with a
+        degree in Geography. My focus on geography and Geographic Information
+        Systems (GIS) opened doors for impact in local government and the
+        nonprofit sector including work with the Grand Valley Metro Council,
+        Grand Rapids Public Museum, and the City of Grand Rapids Engineering
+        Department and Parking Department.
+      </p>
+      <p class="intro-p">
+        So how did I become a Software Engineer? Well my tech journey was
+        non-traditional: in 2019, I completed a coding bootcamp at
+        <span class="flair">Grand Circus</span>. Now, I teach free coding
+        classes through Grand Circus, mentor and connect womxn and BIPOC
+        individuals to opportunities in the tech sector, and maintain a high
+        level of excellence within my field as a successful BIPOC woman in my
+        field. I have the ultimate goal of making the tech sector a more
+        inviting and inclusive place for all.
+      </p>
+      <p class="intro-p">
+        And what about community work? Well, as a member of the 2020 Cohort of
+        Community Catalysts through
+        <span class="flair">WMCAT’s Public Agency</span>, I've utilized
+        Equity-Centered Design to begin the work to challenge and change systems
+        and institutions of power to be more equitable across Grand Rapids
+        through strategic projects and initiatives. I also work on personal
+        community project to create community resources.
+      </p>
+      <p class="signature">
         <span style="font-size: 40px; color: #92140c">&hearts;</span
-        ><span style="font-size: 40px; margin-right: 150px">Jessa</span>
+        ><span style="font-size: 40px">Jessa</span>
       </p>
     </div>
   </v-container>
