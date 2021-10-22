74b3a4e629b831625f8dd9781ced25a7ecc5ca68
Jessa <jessa@mallowfields.com>
2021-10-21 20:50:47 -0400

    WIP adds business directory

--- src/components/BIPOC_Map.vue
+++ src/components/BIPOC_Map.vue
@@ -1,17 +1,157 @@
 <template>
-  <div class="bipoc-page">
-  <div class="d-flex align-center flex-column map-container">
-    <iframe
-      src="https://mallowfields.maps.arcgis.com/apps/instant/interactivelegend/index.html?appid=dbfdb420ccc241b39a0b9a9ca9a59507"
-      class="map"
-      frameborder="0"
-      style="border: 0"
-      allowfullscreen
-      >iFrames are not supported on this page.</iframe
-    >
-   <v-icon size="100px" class="pt-0" color="primary"
-        >mdi-chevron-down</v-icon
-      >
-  </div>
+  <div class="content-body">
+    <div class="bipoc-page">
+      <div class="d-flex align-center flex-column map-container">
+        <iframe
+          src="https://mallowfields.maps.arcgis.com/apps/instant/interactivelegend/index.html?appid=dbfdb420ccc241b39a0b9a9ca9a59507"
+          class="map"
+          frameborder="0"
+          style="border: 0"
+          allowfullscreen
+          >iFrames are not supported on this page.</iframe
+        >
+        <v-icon size="100px" class="pt-0" color="primary"
+          >mdi-chevron-down</v-icon
+        >
+      </div>
+
+      <div class="d-flex align-center flex-column">
+        <div class="d-flex align-center flex-column" style="width: 60%;">
+        <h1>BIPOC Business Directory</h1>
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          item-key="name"
+          class="elevation-1"
+          :search="search"
+        >
+          <template v-slot:top>
+            <v-text-field
+              v-model="search"
+              label="Search"
+              class="mx-4"
+            ></v-text-field>
+          </template>
+        </v-data-table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      search: '',
+      calories: '',
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%'
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%'
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%'
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%'
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: '16%'
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: '0%'
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: '2%'
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: '45%'
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: '22%'
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: '6%'
+        }
+      ]
+    }
+  },
+  computed: {
+    headers () {
+      return [
+        {
+          text: 'Dessert (100g serving)',
+          align: 'start',
+          sortable: false,
+          value: 'name'
+        },
+        {
+          text: 'Calories',
+          value: 'calories',
+          filter: (value) => {
+            if (!this.calories) return true
+
+            return value < parseInt(this.calories)
+          }
+        },
+        { text: 'Fat (g)', value: 'fat' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Iron (%)', value: 'iron' }
+      ]
+    }
+  }
+}
+</script>
