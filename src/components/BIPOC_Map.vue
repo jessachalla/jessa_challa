bd710f238408d64b114d309d4916cb5f19927fef
Jessa Challa <jessa.challa@gmail.com>
2023-06-07 19:22:20 -0400

    updates bipoc page

--- src/components/BIPOC_Map.vue
+++ src/components/BIPOC_Map.vue
@@ -3,26 +3,13 @@
     <div class="bipoc-page">
       <div class="d-flex align-center flex-column map-container">
         <iframe
-          src="https://mallowfields.maps.arcgis.com/apps/instant/interactivelegend/index.html?appid=dbfdb420ccc241b39a0b9a9ca9a59507"
+          src="https://arcg.is/0qGnHK"
           class="map"
           frameborder="0"
           style="border: 0"
           allowfullscreen
           >iFrames are not supported on this page.</iframe
         >
-        <v-icon size="100px" class="pt-0" color="primary"
-          >mdi-chevron-down</v-icon
-        >
-      </div>
-
-      <div class="d-flex align-center flex-column directory-section">
-        <div
-          class="d-flex align-center flex-column bis-directory"
-          style="width: 95%"
-        >
-          <h2>BIPOC Business Directory</h2>
-          <bipoc class="content-body" style="height: 100%"></bipoc>
-        </div>
       </div>
     </div>
     <my-footer class="work-footer"></my-footer>
@@ -30,12 +17,10 @@
 </template>
 
 <script>
-import Bipoc from '@/components/Bipoc.vue'
 import MyFooter from '@/components/Footer'
 
 export default {
   components: {
-    Bipoc,
     MyFooter
   }
 }
